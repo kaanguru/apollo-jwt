f463c62d763b9cc8792112f50ab91a3dc009bfde
netlesh <netlesh@mail.ru>
2020-03-26 20:04:19 +0300

    çalışıyor

---
+++ src/components/selam.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="user in users" :key="user.username">
+        {{ user.username }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import gql from "graphql-tag";
+export default {
+  apollo: {
+    users: gql`
+      {
+          users(where:{blocked: false}, sort: "username", limit:10) {
+    username
+  }
+      }
+    `
+  }
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueApollo from 'vue-apollo'
+import ApolloClient from 'apollo-boost'
 
-Vue.config.productionTip = false
+Vue.config.productionTip = true;
+
+const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiaXNBZG1pbiI6dHJ1ZSwiaWF0IjoxNTg1MDY3MzIzLCJleHAiOjE1ODc2NTkzMjN9.1wRPz1FI5z4b5JGKGEuStfuHBO5ZQVZuejGefrviTfQ";
+
+Vue.use(VueApollo)
+const apolloClient = new ApolloClient({
+  uri: 'http://192.168.0.2:1515/graphql',
+  headers: {
+    authorization: `Bearer ${jwt}`
+  }
+}
+)
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
+
 
 new Vue({
+  apolloProvider,
   render: h => h(App),
 }).$mount('#app')
